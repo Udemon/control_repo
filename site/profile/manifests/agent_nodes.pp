9f65529ae8ca4db99fd6ce1723fdf43f0acb44e9
Udemon <34432112+Udemon@users.noreply.github.com>
2021-11-22 19:32:09 +1100

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,6 +2,7 @@ class profile::agent_nodes{
   include dockeragent
   dockeragent::node{'web.puppet.vm':}
   dockeragent::node{'db.puppet.vm':}
+  dockeragent::node{'mc.puppet.vm':}
   host {'web.puppet.vm':
     ensure       => present,
     ip           => '172.18.0.2',
@@ -11,5 +12,10 @@ class profile::agent_nodes{
     ensure       => present,
     ip           => '172.18.0.3',
     host_aliases => 'dbsrv'
+  }
+  host {'mc.puppet.vm':
+    ensure       => present,
+    ip           => '172.18.0.4',
+    host_aliases => 'mcsrv'
   }
 } 
