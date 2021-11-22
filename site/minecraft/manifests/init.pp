f02cdfe94871ad5e4bbb728d0c6e758631017b3d
Udemon <34432112+Udemon@users.noreply.github.com>
2021-11-22 19:55:33 +1100

    Create init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,20 @@
+class mine {
+  file {'/opt/mc':
+    ensure => directory,
+  }
+  file {'/opt/mc/mc.jar':
+    ensure => present,
+    source => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
+  }
+  package {'java':
+    ensure => present,
+  }
+  file {'/opt/mc/eula.txt':
+    ensure  => present,
+    content => 'eula=true',
+  }
+  file {'/etc/systemd/system/mine.service':
+    ensure => present,
+    source => puppet:///modules/mine/mine.service',
+  }
+}  
